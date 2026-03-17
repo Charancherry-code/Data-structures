3cb4839ecdce590d54f64394cb21f2f800e841f8
Banoth Charan <charancherry1129@gmail.com>
2026-03-17 21:49:50 +0530

    update: improve GridWays backtracking implementation

--- BACK-TRACKING/GridWays/code.cpp
+++ BACK-TRACKING/GridWays/code.cpp
@@ -3,9 +3,10 @@
 #include<vector>
 using namespace std;
 
-int uniquePaths(int r, int c, int n, int m){
+int uniquePaths(int r, int c, int n, int m, string ans){
 
     if(r==n-1 && c==m-1){
+        cout<<ans<<endl;
 
         return 1;
 
@@ -17,10 +18,10 @@ int uniquePaths(int r, int c, int n, int m){
     }
 
     //right move
-    int way1 = uniquePaths(r, c+1, n, m);
+    int way1 = uniquePaths(r, c+1, n, m, ans+'R');
 
     //down side move
-     int way2 = uniquePaths(r+1, c, n, m);
+     int way2 = uniquePaths(r+1, c, n, m, ans+'D');
 
      return way1+ way2;
 }
@@ -29,8 +30,9 @@ int main(){
 
     int n =3;
     int m =3;
+    string ans ="";
 
-    cout<<uniquePaths(0,0, n, m);
+    cout<<uniquePaths(0,0, n, m, ans);
      
 
     return 0;
